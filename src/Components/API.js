b48294dfa4f65feed61e452c40d7e917c154ed35
Vinay-N-07 <vinaynagaraj0@gmail.com>
2024-09-17 01:21:59 +0530

    help

--- src/Components/API.js
+++ src/Components/API.js
@@ -1,13 +1,13 @@
-export const Images = 'http://54.162.114.126:5000/images';
-export const Add_event = 'http://54.162.114.126:5000/add_event';
-export const Approval = 'http://54.162.114.126:5000/approval';
-export const Event_details = 'http://54.162.114.126:5000/eventdetails';
-export const Upload = 'http://54.162.114.126:5000/upload';
-export const Downlad_invite = 'http://54.162.114.126:5000/download_invitation';
-export const Get_data = 'http://54.162.114.126:5000/getUsers';
-export const Add_user = 'http://54.162.114.126:5000/addUser';
-export const Update = 'http://54.162.114.126:5000/update_user_data'; 
-export const Registered_event = 'http://54.162.114.126:5000/RegisteredEvents';
-export const To_register = 'http://54.162.114.126:5000/to_register';
+export const Images = 'http://192.168.1.39:5000/images';
+export const Add_event = 'http://192.168.1.39:5000/add_event';
+export const Approval = 'http://192.168.1.39:5000/approval';
+export const Event_details = 'http://192.168.1.39:5000/eventdetails';
+export const Upload = 'http://192.168.1.39:5000/upload';
+export const Downlad_invite = 'http://192.168.1.39:5000/download_invitation';
+export const Get_data = 'http://192.168.1.39:5000/getUsers';
+export const Add_user = 'http://192.168.1.39:5000/addUser';
+export const Update = 'http://192.168.1.39:5000/update_user_data'; 
+export const Registered_event = 'http://192.168.1.39:5000/RegisteredEvents';
+export const To_register = 'http://192.168.1.39:5000/to_register';
 
 

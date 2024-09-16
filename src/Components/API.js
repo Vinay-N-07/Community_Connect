42bbba03b979a39e384b739182c7c50ca7a2b048
Vinay-N-07 <vinaynagaraj0@gmail.com>
2024-09-17 02:11:19 +0530

    help4

--- src/Components/API.js
+++ src/Components/API.js
@@ -1,13 +1,13 @@
-export const Images = 'http://192.168.1.39:5000/images';
-export const Add_event = 'http://192.168.1.39:5000/add_event';
-export const Approval = 'http://192.168.1.39:5000/approval';
-export const Event_details = 'http://192.168.1.39:5000/eventdetails';
-export const Upload = 'http://192.168.1.39:5000/upload';
-export const Downlad_invite = 'http://192.168.1.39:5000/download_invitation';
-export const Get_data = 'http://192.168.1.39:5000/getUsers';
-export const Add_user = 'http://192.168.1.39:5000/addUser';
-export const Update = 'http://192.168.1.39:5000/update_user_data'; 
-export const Registered_event = 'http://192.168.1.39:5000/RegisteredEvents';
-export const To_register = 'http://192.168.1.39:5000/to_register';
+export const Images = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/images';
+export const Add_event = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/add_event';
+export const Approval = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/approval';
+export const Event_details = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/eventdetails';
+export const Upload = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/upload';
+export const Downlad_invite = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/download_invitation';
+export const Get_data = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/getUsers';
+export const Add_user = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/addUser';
+export const Update = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/update_user_data'; 
+export const Registered_event = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/RegisteredEvents';
+export const To_register = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/to_register';
 
 

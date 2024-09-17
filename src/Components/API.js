f4aa6e224a8659cd67439dd4741cdc3cec7dfcbd
Vinay-N-07 <vinaynagaraj0@gmail.com>
2024-09-18 02:11:24 +0530

    18-09-2024 - Minor feature editing and API modification | 2

--- src/Components/API.js
+++ src/Components/API.js
@@ -1,26 +1,26 @@
-export const Images = 'http://localhost:5000/images';
-export const Add_event = 'http://localhost:5000/add_event';
-export const Approval = 'http://localhost:5000/approval';
-export const Event_details = 'http://localhost:5000/eventdetails';
-export const Upload = 'http://localhost:5000/upload';
-export const Downlad_invite = 'http://localhost:5000/download_invitation';
-export const Get_data = 'http://localhost:5000/getUsers';
-export const Add_user = 'http://localhost:5000/addUser';
-export const Update = 'http://localhost:5000/update_user_data'; 
-export const Registered_event = 'http://localhost:5000/RegisteredEvents';
-export const To_register = 'http://localhost:5000/to_register';
+// export const Images = 'http://localhost:5000/images';
+// export const Add_event = 'http://localhost:5000/add_event';
+// export const Approval = 'http://localhost:5000/approval';
+// export const Event_details = 'http://localhost:5000/eventdetails';
+// export const Upload = 'http://localhost:5000/upload';
+// export const Downlad_invite = 'http://localhost:5000/download_invitation';
+// export const Get_data = 'http://localhost:5000/getUsers';
+// export const Add_user = 'http://localhost:5000/addUser';
+// export const Update = 'http://localhost:5000/update_user_data'; 
+// export const Registered_event = 'http://localhost:5000/RegisteredEvents';
+// export const To_register = 'http://localhost:5000/to_register';
 
-// export const Images = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/images';
-// export const Add_event = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/add_event';
-// export const Approval = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/approval';
-// export const Event_details = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/eventdetails';
-// export const Upload = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/upload';
-// export const Downlad_invite = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/download_invitation';
-// export const Get_data = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/getUsers';
-// export const Add_user = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/addUser';
-// export const Update = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/update_user_data'; 
-// export const Registered_event = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/RegisteredEvents';
-// export const To_register = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/to_register';
+export const Images = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/images';
+export const Add_event = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/add_event';
+export const Approval = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/approval';
+export const Event_details = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/eventdetails';
+export const Upload = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/upload';
+export const Downlad_invite = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/download_invitation';
+export const Get_data = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/getUsers';
+export const Add_user = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/addUser';
+export const Update = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/update_user_data'; 
+export const Registered_event = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/RegisteredEvents';
+export const To_register = 'https://38106mwjsi.execute-api.us-east-1.amazonaws.com/to_register';
 
 
 
